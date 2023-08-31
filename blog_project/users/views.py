243b65a63dcf491a4b734e67976f4c736f58e11e
Peplinn <chidiebubeoluoma01@gmail.com>
2023-08-31 21:54:18 +0100

    Added Edit Profile Functionality, Post Avatar.

--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -17,8 +17,17 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user) #Populates the fields of the forms with instances of the user and their profile
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile) #File attached (e.g. image)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
