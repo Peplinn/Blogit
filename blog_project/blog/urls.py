2b450055cf7ac3c0ff80801851e3fc2eeaf6ef83
Peplinn <chidiebubeoluoma01@gmail.com>
2023-09-01 23:33:44 +0100

    Added CRD Post Functionalites, Authorization.

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #pk - Primary Key/ ID. Can change the name pk by adding an attribute to the class
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'), #<model>/_<form>
     path('about/', views.about, name='blog-about'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,23 +1,29 @@
+from typing import Optional
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 #from django.http import HttpResponse
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Ebube',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 7, 2023'
-    },
-    {
-        'author': 'Peter',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 7, 2023'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Ebube',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 7, 2023'
+#     },
+#     {
+#         'author': 'Peter',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 7, 2023'
+#     }
+# ]
 
 @login_required
 def home(request):
@@ -26,6 +32,49 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+# @method_decorator(login_required, name = 'dispatch') #Functional, but we will replace this with a mixin
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/home.html' # Expected format: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+# @method_decorator(login_required, name = 'dispatch')
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 @login_required
 def about(request):
