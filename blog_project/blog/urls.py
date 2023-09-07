38692e9a27e8844cd0a99370b78e36fcdeeed39d
Peplinn <chidiebubeoluoma01@gmail.com>
2023-09-07 18:20:17 +0100

    Implemented User and All Post Pagination

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #pk - Primary Key/ ID. Can change the name pk by adding an attribute to the class
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
