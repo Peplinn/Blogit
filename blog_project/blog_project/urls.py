a9947846c96b41e8868aea972e56658888cc869a
Chidiebube Oluoma <chidiebubeoluoma01@gmail.com>
2024-09-25 22:20:26 +0100

    Redesign Initialization

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 from users import views as user_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('p3p0n1y/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
