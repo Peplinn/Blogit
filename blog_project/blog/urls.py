3dbdb559d033a603e7c418350c32815490ba2c14
Oluoma Chidiebube <100541154+Peplinn@users.noreply.github.com>
2024-09-29 22:52:21 +0100

    Revert "Staging"

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #pk - Primary Key/ ID. Can change the name pk by adding an attribute to the class
-    # path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    # path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    # path('post/new/', PostCreateView.as_view(), name='post-create'), #<model>/_<form>
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'), #<model>/_<form>
     path('about/', views.about, name='blog-about'),
 ]
